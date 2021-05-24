9cfaef93d6c8dba7db48c9c0b38a67f4350e6a0a
Nadir Pervez <nadirpak95@hotmail.com>
2021-05-24 12:51:22 +0300

    added read/write to binary file
    added generic functions to read/write  binary file  on whatever the type is.

---
+++ Assets/Scripts/Controllers-Managers/SaveDataManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
+
+public class SaveDataManager : MonoBehaviour
+{
+    public Text text; 
+    private void Awake()
+    {
+        SaveData saveData = new SaveData();
+        saveData.player = "pain";
+        string _saveFilePath = Application.persistentDataPath + "/savedata.data"; //for SaveData class
+
+        WriteToBinary(saveData,_saveFilePath);
+        ReadFromBinaryPlayerData(_saveFilePath);
+    }
+
+    private void WriteToBinary(SaveData saveDataModel, string saveFilePath)
+    {
+        Debug.Log(saveFilePath);
+        WriteToBinaryFile(saveFilePath, saveDataModel);
+    }
+
+    private void ReadFromBinaryPlayerData(string saveFilePath)
+    {
+      SaveData playerData = ReadFromBinaryFile<SaveData>(saveFilePath);
+     // Debug.Log(playerData.ToString());
+      text.text = playerData.ToString();
+    }
+    public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
+    {
+        using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
+        {
+            var binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(stream, objectToWrite);
+        }
+    }
+
+    public static T ReadFromBinaryFile<T>(string filePath)
+    {
+        using (Stream stream = File.Open(filePath, FileMode.Open))
+        {
+            var binaryFormatter = new BinaryFormatter();
+            return (T) binaryFormatter.Deserialize(stream);
+        }
+    }
+}
---
+++ Assets/Scripts/DataModels/SaveData.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+public class SaveData
+{
+    public string objID { get; set; }
+    public string player { get; set; }
+    public string[] unlockedCheckpointIDs { get; set; }
+    public int[] lastPosition { get; set; }
+    public int hp { get; set; }
+    public int mana { get; set; }
+    public int anger { get; set; }
+
+    public SaveData() //default constructor 
+    {
+        objID = "n/a";
+        player = "n/a";
+        unlockedCheckpointIDs = new string[0];
+        lastPosition = new int[3];
+        hp = 100;
+        mana = 100;
+        anger = 0;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append(" save data info--> ");
+        stringBuilder.Append(" objID--> ");
+        stringBuilder.Append(objID);
+        stringBuilder.Append(" player--> ");
+        stringBuilder.Append(player);
+        stringBuilder.Append(" unlockedCheckpoints array--> ");
+        for (int i = 0; i < unlockedCheckpointIDs.Length; i++)
+        {
+            stringBuilder.Append(unlockedCheckpointIDs[i]);
+            stringBuilder.Append(" ");
+        }
+        stringBuilder.Append(" last position--> ");
+        for (int i = 0; i < lastPosition.Length; i++)
+        {
+            stringBuilder.Append(lastPosition[i]);
+            stringBuilder.Append(" ");
+        }
+        stringBuilder.Append(" hp--> ");
+        stringBuilder.Append(hp);
+        stringBuilder.Append(" mana--> ");
+        stringBuilder.Append(mana);
+        stringBuilder.Append(" anger--> ");
+        stringBuilder.Append(anger);
+        return stringBuilder.ToString();
+    }
+}

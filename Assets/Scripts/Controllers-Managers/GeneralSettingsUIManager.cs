95cc00315ecd3e344b3983dd338fa17a902d796e
Nadir Pervez <nadirpak95@hotmail.com>
2021-05-31 23:15:54 +0300

    added saving to playerprefs

--- Assets/Scripts/Controllers-Managers/GeneralSettingsUIManager.cs
+++ Assets/Scripts/Controllers-Managers/GeneralSettingsUIManager.cs
@@ -2,64 +2,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Text;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class GeneralSettingsUIManager : MonoBehaviour
 {
     [SerializeField] private GameObject _settingsBodyPanel;
+    [SerializeField] private ResolutionSettingsUI _resolutionSettingsUI;
 
-    private SettingsParametersComponent[] _settingsParametersComponents;
+    [SerializeField] private Button _saveButton;
+    // private SettingsParametersComponent[] _settingsParametersComponents;
 
     // Start is called before the first frame update
-    private void Awake()
+    private void Start()
     {
         Initialize();
     }
 
     private void Initialize()
     {
-        // //get all components 
-        // int childCount = _settingsBodyPanel.transform.GetComponentsInChildren<SettingsParametersComponent>().Length;
-        // _settingsParametersComponents = new SettingsParametersComponent[childCount];
-        // _settingsParametersComponents =
-        //     _settingsBodyPanel.transform.GetComponentsInChildren<SettingsParametersComponent>();
-        // Debug.Log("_settingsParametersComponents " + _settingsParametersComponents.Length);
-        //assign events 
-        
+        _saveButton.onClick.AddListener(delegate { WriteGraphicsSettings(_resolutionSettingsUI.graphicsSettings); });
+        // Debug.Log(Screen.currentResolution.ToString());
     }
 
-    private void LinkEvents()
+    //read from player prefs
+    private void LinkGraphicsSettings(SettingsParametersComponent[] graphicsSettings)
     {
-    
+        //read from player prefs and set everything on initialize 
+        //load graphics 
+       
+        //
     }
-    
-    private void SaveGraphicsChanges(Text[] graphicsSettings) 
+
+    //Apply Graphics Settings Here!! 
+    public void WriteGraphicsSettings(SettingsParametersComponent[] graphicsSettings)
     {
+        string resultString;
+        FullScreenMode screenMode;
+
         for (int i = 0; i < graphicsSettings.Length; i++)
         {
-            switch (graphicsSettings[i].text)
+            switch (graphicsSettings[i].componentTag)
             {
                 case "_res":
+                    string[] result = graphicsSettings[i].parameterText.text.Split('x');
+                    int width, height = 0;
+                    Debug.Log("result string save " + result[0]);
+                    width = int.Parse(result[0]);
+                    height = int.Parse(result[1]);
+                    Screen.SetResolution(width, height, FullScreenMode.TryParse(graphicsSettings[1].parameterText.text, out screenMode));
+                    Debug.Log("result " + result[0]);
+                    //save into player prefs
+                    break;
+                case "_wm":
+                    resultString = graphicsSettings[i].parameterText.text;
+                    bool IsParsed;
+
+                    IsParsed = FullScreenMode.TryParse(resultString, out screenMode); //platform specific class 
+                    if (IsParsed)
+                    {
+                        Debug.Log("screen mode " + screenMode);
+                        Screen.fullScreenMode = screenMode;
+                    }
+                    break;
+                case "_v_sync":
+                    int v_syncIndex = Array.IndexOf(graphicsSettings[i].values, graphicsSettings[i].parameterText.text);
+                    Debug.Log("index of v sync " + v_syncIndex);
+                    QualitySettings.vSyncCount = v_syncIndex;  // 0 disable vsync, 1 enable v_sync 
+                    break;
+                case "_mquality":  //material Quality 
                     
-                    break; 
-                case  "_wm":
                     break;
-                case  "_v_sync":
-                    break;
-                case  "_mquality":
-                    break;
-                case  "_msaa":
+                case "_msaa":
+                    //needs to be searched in settings 
+
                     break;
                 case "_texquality":
-                    break;
-                case "_shadows" :
+                    //needs to be searched in settings 
+
+                    break; 
+                case "_shadows":
+                    //needs to be searched in settings 
+
                     break;
             }
+            PlayerPrefs.SetString(graphicsSettings[i].componentTag,graphicsSettings[i].parameterText.text);
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
     }
 }
--- Assets/Scripts/UI/ResolutionSettingsUI.cs
+++ Assets/Scripts/UI/ResolutionSettingsUI.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,14 +11,32 @@ public class ResolutionSettingsUI : MonoBehaviour
 {
     [SerializeField] private GameObject _resolutionSettingsBodyPanel;
     [SerializeField] private HDRenderPipelineAsset _hdRenderPipelineAsset;
+    [SerializeField] private GeneralSettingsUIManager _generalSettingsUIManager;
     private SettingsParametersComponent[] _settingsParametersComponents;
     private Text[] _parametersValues;
 
-    // Start is called before the first frame update
-    void Awake()
+    public SettingsParametersComponent[] graphicsSettings
     {
+        get { return _settingsParametersComponents; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // PlayerPrefs.DeleteAll();
         Initialize();
-        SetGraphicsSettings();//on UI
+        SetGraphicsSettings(); //on UI to be assigned to each choice [Text], fill the UI
+        //read settings and assign index to each choice 
+
+        LinkGraphicsSettings(graphicsSettings);
+        StartCoroutine(ApplyGraphicsSettings());
+    }
+
+    IEnumerator ApplyGraphicsSettings()
+    {
+        yield return new WaitForSeconds(0.0f);
+        _generalSettingsUIManager.WriteGraphicsSettings(graphicsSettings);
+
     }
 
     private void Initialize()
@@ -35,18 +55,96 @@ public class ResolutionSettingsUI : MonoBehaviour
         {
             _parametersValues[i] = _settingsParametersComponents[i].parameterText; // pass as reference 
         }
-        //load saved values from general settings manager
-
-        //
     }
 
-    //set values on UI
-    private void SetGraphicsSettings() 
+    private void LinkGraphicsSettings(SettingsParametersComponent[] graphicsSettings)
+    {
+        //read from player prefs and set everything on initialize 
+        //load graphics 
+        string outputPlayerPrefs = "";
+        for (int i = 0; i < graphicsSettings.Length; i++)
+        {
+            outputPlayerPrefs =
+                PlayerPrefs.GetString(graphicsSettings[i].componentTag);
+            Debug.Log("outputPlayerPrefs " + outputPlayerPrefs);
+            if (string.IsNullOrEmpty(outputPlayerPrefs))
+            {
+                //make initial values 
+                Debug.Log(
+                    "string.IsNullOrEmpty(outputPlayerPrefs) && !PlayerPrefs.HasKey(graphicsSettings[i].componentTag))");
+                SetDefaultPlayerPrefs(graphicsSettings[i]);
+            }
+
+            if (PlayerPrefs.HasKey(graphicsSettings[i].componentTag))
+            {
+                outputPlayerPrefs =
+                    PlayerPrefs.GetString(graphicsSettings[i].componentTag);
+                //set the index for each setting 
+                LinkPlayerPrefsToGraphicsSettings(outputPlayerPrefs, graphicsSettings[i]);
+            }
+            
+        }
+        
+    }
+
+    private void LinkPlayerPrefsToGraphicsSettings(string outputPlayerPrefs,
+        SettingsParametersComponent graphicsSetting)
+    {
+        int index = Array.IndexOf(graphicsSetting.values, outputPlayerPrefs);
+        Debug.Log("LinkPlayerPrefsToGraphicsSettings index " + index);
+        graphicsSetting.index = index;
+       
+    }
+
+    private void SetDefaultPlayerPrefs(SettingsParametersComponent graphicsSetting) //set strings to player prefs 
     {
         StringBuilder stringBuilder = new StringBuilder();
-        FullScreenMode[] fullScreenOptions = {
+        switch (graphicsSetting.componentTag)
+        {
+            case "_res":
+                stringBuilder.Append(Screen.currentResolution.width + "x" + Screen.currentResolution.height);
+                PlayerPrefs.SetString(graphicsSetting.componentTag, stringBuilder.ToString());
+                // Screen.SetResolution(1280, 720, FullScreenMode.ExclusiveFullScreen);
+                stringBuilder.Clear();
+                break;
+            case "_wm":
+                stringBuilder.Append(FullScreenMode.ExclusiveFullScreen.ToString());
+                // Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                PlayerPrefs.SetString(graphicsSetting.componentTag, stringBuilder.ToString());
+                stringBuilder.Clear();
+                break;
+            case "_v_sync":
+                // QualitySettings.vSyncCount = 1;
+                PlayerPrefs.SetString(graphicsSetting.componentTag, "ON");
+                break;
+            case "_mquality":
+
+                break;
+            case "_msaa":
+                //needs to be searched in settings 
+
+                break;
+            case "_texquality":
+                //needs to be searched in settings 
+
+                break;
+            case "_shadows":
+                //needs to be searched in settings 
+
+                break;
+        }
+    }
+
+
+    //fill  values on UI [nothing more]
+    private void SetGraphicsSettings()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        FullScreenMode[] fullScreenOptions =
+        {
             FullScreenMode.ExclusiveFullScreen,
-            FullScreenMode.FullScreenWindow, //FullScreenWindow = BorderlessFullscreen. MaximizedWindow is a MacOS-only thing.
+            FullScreenMode
+                .FullScreenWindow, //FullScreenWindow = BorderlessFullscreen. MaximizedWindow is a MacOS-only thing.
             FullScreenMode.Windowed
         };
         for (int i = 0; i < _settingsParametersComponents.Length; i++)
@@ -55,6 +153,7 @@ public class ResolutionSettingsUI : MonoBehaviour
             {
                 case "_res":
                     _settingsParametersComponents[i].values = new string[Screen.resolutions.Length];
+                    
                     for (int j = 0; j < _settingsParametersComponents[i].values.Length; j++)
                     {
                         stringBuilder.Append(Screen.resolutions[j].width);
@@ -63,6 +162,11 @@ public class ResolutionSettingsUI : MonoBehaviour
                         _settingsParametersComponents[i].values[j] = stringBuilder.ToString();
                         stringBuilder.Clear();
                     }
+
+                    string[] distinctValues = _settingsParametersComponents[i].values.Distinct().ToArray();
+;
+                    _settingsParametersComponents[i].values = distinctValues;
+
                     break;
                 case "_wm":
                     _settingsParametersComponents[i].values = new string[fullScreenOptions.Length];
@@ -72,6 +176,7 @@ public class ResolutionSettingsUI : MonoBehaviour
                         _settingsParametersComponents[i].values[j] = stringBuilder.ToString();
                         stringBuilder.Clear();
                     }
+
                     break;
                 case "_v_sync":
                     _settingsParametersComponents[i].values = new string[2];

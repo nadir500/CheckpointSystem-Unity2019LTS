5a44c29461bfc92176021cce32143933b546d662
Nadir Pervez <nadirpak95@hotmail.com>
2021-06-14 11:38:19 +0300

    grass shader added with wind effects and coloring

---
+++ Assets/Scripts/ShaderInteractionScripts/GrassInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class GrassInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material mat;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Shader.SetGlobalVector("_PlayerPos", transform.position);
+       mat.SetVector("_PlayerPos",transform.position);
+    }
+}
